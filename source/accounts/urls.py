f41b5cb817352738363b5044a038c32b0841107a
GLeb Tarakanov <glebtgd@gmail.com>
2023-04-22 13:43:23 +0600

    #4 Реализована аутентификацию и подключено приложение accounts
    Работают три страницы: login, registration, logout

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 
+from accounts.views import RegisterView, LoginView, logout_view
+
 urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', logout_view, name='logout'),
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from django.views.generic import CreateView, TemplateView
+
+from accounts.forms import CustomUserCreationForm, LoginForm
+
 
 # Create your views here.
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+    form = LoginForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if not form.is_valid():
+            return redirect('index')
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return redirect('index')
+        next = request.GET.get('next')
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('index')
+
+
+class RegisterView(CreateView):
+    template_name = 'registration.html'
+    form_class = CustomUserCreationForm
+    success_url = '/'
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(self.success_url)
+        context = {'form': form}
+        return self.render_to_response(context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
