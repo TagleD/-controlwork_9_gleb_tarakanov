3a399f8380945dac217018cce68d0475bf25ad1f
GLeb Tarakanov <glebtgd@gmail.com>
2023-04-22 15:18:36 +0600

    #4 Сделал фикстуры для приложения accounts и webapp

--- source/webapp/views/photos.py
+++ source/webapp/views/photos.py
@@ -10,7 +10,7 @@ class IndexView(ListView):
     template_name = 'photo_list.html'
     model = Photo
     context_object_name = 'photos'
-    ordering = ('created_at',)
+    ordering = ('-created_at',)
 
 
 class PhotoCreateView(CreateView):
